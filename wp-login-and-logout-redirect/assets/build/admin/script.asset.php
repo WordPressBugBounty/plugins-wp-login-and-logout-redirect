60083dde0f0529137257c07a7ee3fdeb9b2d626c
Sean Murphy <sean@iamseanmurphy.com>
2026-07-17 22:03:28 +0000

    4.0.0

---
+++ wp-login-and-logout-redirect/assets/build/admin/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => '01c509905518ed6f2155');
--- wp-login-and-logout-redirect/vendor/composer/installed.php
+++ wp-login-and-logout-redirect/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'pluginizelab/wp-login-logout-redirect',
-        'pretty_version' => '3.1.7',
-        'version' => '3.1.7.0',
-        'reference' => 'fcc2c51207c2af8c75de4d6950fcd2f8107b8126',
+        'pretty_version' => 'v4.0.0',
+        'version' => '4.0.0.0',
+        'reference' => '255a19c2e48f32b771c4edfeb4738d9dba4878ce',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'pluginizelab/wp-login-logout-redirect' => array(
-            'pretty_version' => '3.1.7',
-            'version' => '3.1.7.0',
-            'reference' => 'fcc2c51207c2af8c75de4d6950fcd2f8107b8126',
+            'pretty_version' => 'v4.0.0',
+            'version' => '4.0.0.0',
+            'reference' => '255a19c2e48f32b771c4edfeb4738d9dba4878ce',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
